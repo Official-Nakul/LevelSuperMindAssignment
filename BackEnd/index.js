1ffca4d6dc95b2b00dd6c7c615347e8c2c20ca0c
Official-Nakul <visavadianakulyt@gmail.com>
2025-01-06 21:27:21 +0530

    backend update

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,21 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
-
 const app = express();
 
-// Import routes
 const chatRoute = require("./Routes/chatRoute");
-/*const graphDataRoute = require("./Routes/graphDataRoute");*/
 
-// Middleware setup
-app.use(cors()); // Allow requests from the frontend
-app.use(bodyParser.json()); // Parse JSON request bodies
+app.use(cors());
+app.use(bodyParser.json());
 
-// Routes setup
-app.use("/api", chatRoute); // Chat route
-/*app.use("/api", graphDataRoute); // Graph data route*/
-// Start the server
+app.use("/api", chatRoute);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
