4e87f54f452fd2f9734d42caf9fb5b9142193dc4
Official-Nakul <visavadianakulyt@gmail.com>
2025-01-10 19:54:08 +0530

    final commit

--- FrontEnd/src/components/DashBoard/DashBoard.jsx
+++ FrontEnd/src/components/DashBoard/DashBoard.jsx
@@ -1,28 +1,25 @@
 import { BarChartComp } from "./BarChartComp";
 import { PieChartComp } from "./PieChartComp";
 import { DataDisplay } from "./DataDisplay";
-import { CsvProvider } from "./CsvContext";
 import { LineChart } from "./LineChart";
 function DashBoard() {
   return (
     <>
       <div className="flex flex-1 flex-col gap-4">
-        <CsvProvider>
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className=" rounded-xl bg-muted/50">
-              <BarChartComp />
-            </div>
-            <div className=" rounded-xl bg-muted/50">
-              <PieChartComp />
-            </div>
-            <div className=" rounded-xl bg-muted/50 h-full flex justify-center items-center">
-              <LineChart />
-            </div>
+        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+          <div className=" rounded-xl bg-muted/50">
+            <BarChartComp />
           </div>
-          <div className="min-h-full flex-1 rounded-xl bg-muted/50 md:min-h-min">
-            <DataDisplay />
+          <div className=" rounded-xl bg-muted/50">
+            <PieChartComp />
           </div>
-        </CsvProvider>
+          <div className=" rounded-xl bg-muted/50 h-full flex justify-center items-center">
+            <LineChart />
+          </div>
+        </div>
+        <div className="min-h-full flex-1 rounded-xl bg-muted/50 md:min-h-min">
+          <DataDisplay />
+        </div>
       </div>
     </>
   );
